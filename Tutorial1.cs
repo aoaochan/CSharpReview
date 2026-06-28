25e9f0fce5b1e03e8a4736807a56e8897337bd9d
aoaochan <josejbeck99@yahoo.com>
2026-06-29 08:51:54 +0900

    Tutorial2: Complete the tutorial.

---
+++ Tutorial1.cs
@@ -0,0 +1,42 @@
+﻿// [첫 번째 C# 코드 작성]
+// https://learn.microsoft.com/ko-kr/training/modules/csharp-write-first/1-introduction
+
+namespace CSharpReview;
+
+class Tutorial1
+{
+    public static void LessonFirst()
+    {
+        // [연습 - 첫 번째 코드 작성]
+        // https://learn.microsoft.com/ko-kr/training/modules/csharp-write-first/2-exercise-hello-world
+
+        // Console.WriteLine("Hello, World!");
+
+        // Console.Write("Congratulations!");
+        // Console.Write(" ");
+        // Console.Write("You wrote your first lines of code.");
+
+        Console.WriteLine("Congratulations!");
+        Console.Write("You wrote your first lines of code.");
+    }
+
+    public static void LessonSecond()
+    {
+        // [과제]
+        // https://learn.microsoft.com/ko-kr/training/modules/csharp-write-first/4-challenge
+
+        Console.WriteLine("This is the first line.\nThis is the second line.");
+    }
+
+    public static void LessonThird()
+    {
+        // [솔루션 검토]
+        // https://learn.microsoft.com/ko-kr/training/modules/csharp-write-first/5-solution
+
+        Console.WriteLine("This is the first line.");
+
+        Console.Write("This is ");
+        Console.Write("the second ");
+        Console.Write("line.");
+    }
+}
---
+++ Tutorial2.cs
@@ -0,0 +1,92 @@
+// [C에서 리터럴 및 변수 값을 사용하여 데이터 저장 및 검색#]
+// https://learn.microsoft.com/ko-kr/training/modules/csharp-literals-variables/
+
+namespace CSharpReview;
+
+class Tutorial2
+{
+    public static void LessonFirst()
+    {
+        // [연습 - 리터럴 값 출력]
+        // https://learn.microsoft.com/ko-kr/training/modules/csharp-literals-variables/2-exercise-literal-values
+
+        // Console.WriteLine('b'); // Okay
+        // Console.WriteLine('Hello, world!'); // Error!
+        // Console.WriteLine(123);
+
+        /// Float Type - Precision
+        /// float        ~6-9 digits
+        /// double       ~15-17 digits
+        /// decimal      ~28-29 digits
+        // Console.WriteLine(0.25f); // float
+        // Console.WriteLine(2.625); // double
+        // Console.WriteLine(12.39816m); // decimal
+
+        // Console.WriteLine(true);
+        // Console.WriteLine(false);
+
+        Console.WriteLine("123");
+        Console.WriteLine(123);
+        Console.WriteLine("true");
+        Console.WriteLine(true);
+    }
+
+    public static void LessonSecond()
+    {
+        // [변수 선언]
+        // https://learn.microsoft.com/ko-kr/training/modules/csharp-literals-variables/3-declaring-variables
+
+        // string firstName;
+        // char userOption;
+        // int gameScore;
+        // decimal particlesPerMillion;
+        // bool processedCustomer;
+    }
+
+    public static void LessonThird()
+    {
+        // 연습 - 변수 값 설정 및 가져오기
+        // https://learn.microsoft.com/ko-kr/training/modules/csharp-literals-variables/4-exercise-variables
+
+        // string firstName;
+        // firstName = "Bob";
+        // Console.WriteLine(firstName);
+        // firstName = "Liem";
+        // Console.WriteLine(firstName);
+        // firstName = "Isabella";
+        // Console.WriteLine(firstName);
+        // firstName = "Yasmin";
+        // Console.WriteLine(firstName);
+
+        // string firstName;
+        // Console.WriteLine(firstName); // Error!
+        
+        string firstName = "Bob";
+        Console.WriteLine(firstName);
+    }
+
+    public static void LessonFourth()
+    {
+        // [암시적 형식 지역 변수 선언]
+        // https://learn.microsoft.com/ko-kr/training/modules/csharp-literals-variables/5-implicitly-typed-local-variables
+
+        // var message = "hello"; // string
+
+        // var message = "Hello, World!";
+        // message = 10.703m; // Error!
+
+        // var message; // Error!
+    }
+
+    public static void LessonFifth()
+    {
+        // [과제]
+        // https://learn.microsoft.com/ko-kr/training/modules/csharp-literals-variables/6-challenge
+
+        string firstName = "Bob";
+        int messages = 3;
+        decimal celsius = 34.4m;
+
+        Console.WriteLine($"Hello, {firstName}! You have {messages} messages in your inbox. The temperature is {celsius} celsius.");
+    }
+}
